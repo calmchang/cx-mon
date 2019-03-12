32b91af9e8600b91d0897e9b6171d0a3dd810854
changxu <>
2019-03-12 13:54:25 +0800

    no message

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/mon.js',
+  output: {
+    libraryTarget:'umd',
+    umdNamedDefine: true,
+    path: path.resolve(__dirname, 'lib'),
+    filename: 'mon.js'
+  },
+  module: {
+    rules: [
+      {test: /\.js$/, loader: 'babel-loader'}
+    ]
+  }
+};
